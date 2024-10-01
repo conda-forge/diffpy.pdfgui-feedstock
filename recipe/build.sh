3e918aeca61ab95e32eb8f00ae6b9f9dcca3eb41
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-10-01 00:15:30 -0400

    Remove build.sh

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#$PYTHON -m easy_install --no-deps .
-$PYTHON -m pip install . --no-deps -vv
-
-# Add more build steps here, if they are necessary.
-
-# See http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
